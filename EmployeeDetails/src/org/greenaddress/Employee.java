607dac49b9211e55f7111130387987bf4642c0f5
Navaneetha-Chandrasekaran <146407608+Navaneetha-Chandrasekaran@users.noreply.github.com>
2024-09-23 16:02:12 +0530

    Add files via upload

---
+++ EmployeeDetails/src/org/greenaddress/Employee.java
@@ -0,0 +1,38 @@
+package org.greenaddress;
+
+
+
+public class Employee {
+         public void empID() {
+        	 System.out.println("e21cs033");
+         }
+         private void empName() {
+			// TODO Auto-generated method stub
+              System.out.println("Navaneetha");
+		}
+         private void empDob() {
+			// TODO Auto-generated method stub
+                System.out.println("06.05.2004");
+		}
+         private void empPhone() {
+ 			// TODO Auto-generated method stub
+                 System.out.println("qwerty");
+ 		}
+         private void empEmail() {
+ 			// TODO Auto-generated method stub
+                 System.out.println("email");
+ 		}
+         private void empAddress() {
+ 			// TODO Auto-generated method stub
+                 System.out.println("address");
+ 		}
+         public static void main(String[] args) {
+			Employee a= new Employee();
+			a.empID();
+			a.empName();
+			a.empDob();
+			a.empPhone();
+			a.empEmail();
+			a.empAddress();
+		}
+}
