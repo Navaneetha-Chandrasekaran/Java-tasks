2cfe55e8e58a46580f5fe2761be21b6b2af9d721
Navaneetha-Chandrasekaran <146407608+Navaneetha-Chandrasekaran@users.noreply.github.com>
2024-09-23 15:39:09 +0530

    Add files via upload

---
+++ CompanyDetails/src/org/companydetails/CompanyInfo.java
@@ -0,0 +1,22 @@
+package org.companydetails;
+
+public class CompanyInfo {
+ private void companyId() {
+	// TODO Auto-generated method stub
+          System.out.println("companyid");
+}
+ private void companyName() {
+		// TODO Auto-generated method stub
+	          System.out.println("companyname");
+	}
+ private void companyAddress() {
+		// TODO Auto-generated method stub
+	          System.out.println("companyAddress");
+	}
+ public static void main(String[] args) {
+	CompanyInfo c= new CompanyInfo();
+	c.companyId();
+	c.companyAddress();
+	c.companyName();
+}
+}
