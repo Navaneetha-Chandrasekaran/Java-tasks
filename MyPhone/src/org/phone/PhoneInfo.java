265f12fad0cc45c8db88bad361608aa1fc1b8bad
Navaneetha-Chandrasekaran <146407608+Navaneetha-Chandrasekaran@users.noreply.github.com>
2024-09-23 16:08:37 +0530

    Add files via upload

---
+++ MyPhone/src/org/phone/PhoneInfo.java
@@ -0,0 +1,29 @@
+package org.phone;
+
+public class PhoneInfo {
+ private void phoneNum() {
+          System.out.println("90807089");
+
+}
+ private void phoneMieiNumber() {
+     System.out.println("vdgvbj+");
+}
+ private void phonestorage () {
+     System.out.println("vdgvbj+");
+}
+ private void camera() {
+     System.out.println("camera");
+}
+ private void osName() {
+     System.out.println("windows");
+}
+ public static void main(String[] args) {
+	 PhoneInfo p= new PhoneInfo();
+	 p.phoneNum();
+	 p.phonestorage();
+	 p.phoneMieiNumber();
+	 p.osName();
+	 p.camera();
+	 
+ }
+}
