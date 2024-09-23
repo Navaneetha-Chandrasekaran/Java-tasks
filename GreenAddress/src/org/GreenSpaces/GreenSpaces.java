58eb0f6b25621d2fa59d2caf2fc195963cb4f000
Navaneetha-Chandrasekaran <146407608+Navaneetha-Chandrasekaran@users.noreply.github.com>
2024-09-23 16:07:20 +0530

    Add files via upload

---
+++ GreenAddress/src/org/GreenSpaces/GreenSpaces.java
@@ -0,0 +1,27 @@
+package org.GreenSpaces;
+
+public class GreenSpaces {
+    private void greenOmr() {
+		System.out.println("OMR");
+	}
+    private void greenAdayar() {
+		System.out.println("ADAYAR");
+	}
+    private void greenVelacherry() {
+		System.out.println("Velacherry");
+	}
+    private void greenTambaram() {
+		System.out.println("Tambarm");
+	}
+    private void greenAnnanagar() {
+		System.out.println("AnnaNagar");
+	}
+    public static void main(String[] args) {
+		GreenSpaces g= new GreenSpaces();
+		g.greenOmr();
+		g.greenAdayar();
+		g.greenTambaram();
+		g. greenVelacherry();
+		g.greenAnnanagar();
+	}
+} 
